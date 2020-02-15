b262c7069cbc52b4e81636c36d2d09e29aa95113
bcakir00 <berkan.cakir@student.hu.nl>
2020-02-15 20:49:06 +0100

    Add initial skeleton for game.py.

---
+++ game.py
@@ -0,0 +1,25 @@
+import algoritmes
+import code
+
+
+number_of_rounds = None
+number_of_pawns = None
+number_of_colors = None
+
+
+def check_if_game_ended():
+    pass
+
+
+def check_if_game_won():
+    pass
+
+
+def game():
+    pass
+
+
+number_of_rounds = input("Please input number of rounds: ")
+number_of_pawns = input("Please input number of pawns (code length): ")
+number_of_colors = input("Please input number of color (code depth): ")
+game()
