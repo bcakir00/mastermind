c4c4d2777f6b8c5a545598da571e19c603a8b12f
bcakir00 <berkan.cakir@student.hu.nl>
2020-02-16 12:05:57 +0100

    Refactor module name and add functions.

--- input_output.py
+++
@@ -1,6 +0,0 @@
-def get_integer_input(input):
-    pass
-
-
-def get_string_input(input):
-    pass
---
+++ io.py
@@ -0,0 +1,15 @@
+def range_validation(data, min, max):
+    if len(data) > min and len(data) < max:
+        return True
+    else:
+        return False
+
+
+def type_validation(actual_type, desired_type):
+    pass
+
+
+def length_validation(length, max):
+    pass
+
+
